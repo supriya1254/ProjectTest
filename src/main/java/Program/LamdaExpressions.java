3d0f4640518afc4e18c84a07f3e22fc693d6fdb6
Supriya Soni <supriyasoni1254@gmail.com>
2020-11-04 12:37:05 +0200

    final commit

--- src/main/java/Program/LamdaExpressions.java
+++ src/main/java/Program/LamdaExpressions.java
@@ -13,33 +13,6 @@ class MyMath {
         return p.check(num);
     }
 
-    /*public static PerformOperation is_odd() {
-        return n -> (n & 1) == 1;
-    }
-
-    public static PerformOperation is_prime() {
-        return n -> {
-            if (n < 2) {
-                return false;
-            }
-            int sqrt = (int) Math.sqrt(n);
-            for (int i = 2; i <= sqrt; i++) {
-                if (n % i == 0) {
-                    return false;
-                }
-            }
-            return true;
-        };
-    }
-
-    public static PerformOperation is_palindrome() {
-        return n -> {
-            String original = Integer.toString(n);
-            String reversed = new StringBuilder(Integer.toString(n)).reverse().toString();
-            return original.equals(reversed);
-        };
-    }*/
-
     public static PerformOperation is_odd(){
         return (int a) -> a % 2 != 0;
     }
